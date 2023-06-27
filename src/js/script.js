7e13dbaa1032c773050f9f12a03d60812b8365d9
Yazan Najem <yazannajem@icloud.com>
2023-06-28 03:21:59 +0400

    modified counter and added whatsapp icon into footer

--- src/js/script.js
+++ src/js/script.js
@@ -61,3 +61,19 @@ if (currentTheme) {
 let myDate = document.querySelector("#datee");
 const yes = new Date().getFullYear();
 myDate.innerHTML = yes;
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
